f823d84e7fc25e7c1ba315f9a07d9110df83684f
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2022-04-26 11:44:09 -0600

    returning a default message when an empty string is passed to hello

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sam")
-	want := "Hello, Sam"
-
-	if want != got {
-		t.Errorf("got %q, want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, want, got string) {
+		t.Helper()
+		if want != got {
+			t.Errorf("want %q, got %q", want, got)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Sam")
+		want := "Hello, Sam"
+		assertCorrectMessage(t, want, got)
+	})
+	t.Run("say 'Hello, World' when an emtpy string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, want, got)
+	})
 }
