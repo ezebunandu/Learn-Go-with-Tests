49e0779f32884e07ec514f449c342d98903583e5
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2022-05-03 16:47:16 -0600

    a few more ways to sum slices

--- sum/sum.go
+++ sum/sum.go
@@ -1,9 +1,32 @@
 package sum
 
 func Sum(numbers []int) (sum int) {
+	if len(numbers) == 0 {
+		return 0
+	}
 	sum = 0
 	for _, num := range numbers {
 		sum += num
 	}
 	return sum
+}
+
+func SumAll(numbersToAdd ...[]int) []int {
+	result := []int{}
+	for _, numbers := range numbersToAdd {
+		result = append(result, Sum(numbers))
+	}
+	return result
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	result := []int{}
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			result = append(result, 0)
+		} else {
+			result = append(result, Sum(numbers[1:]))
+		}
+	}
+	return result
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,6 +3,8 @@ package sum_test
 import (
 	"sum"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestSum(t *testing.T) {
@@ -14,5 +16,34 @@ func TestSum(t *testing.T) {
 		if want != got {
 			t.Errorf("want %d, got %d, given %#v", want, got, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := sum.SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !cmp.Equal(want, got) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, want, got []int) {
+		t.Helper()
+		if !cmp.Equal(want, got) {
+			t.Errorf("want %v, got %v", got, want)
+		}
+
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := sum.SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, want, got)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := sum.SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, want, got)
 	})
 }
